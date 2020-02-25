fc09d52fae6b41a1519fb4c4fbbd58415171654e
Ivan Palacios <ipalacios4@ucmerced.edu>
2020-02-24 22:47:51 -0800

    Still working on MatrixMult (SEG Fault)

--- Lab_4/MatrixMult.c
+++ Lab_4/MatrixMult.c
@@ -4,30 +4,47 @@
 
 int** matMult(int **a, int **b, int size){
 	// (4) Implement your matrix multiplication here. You will need to create a new matrix to store the product.
-
+	int **mult;
+	int i =0, j = 0;
+	for(i; i<size; i++){
+		for(j; j<size; j++){
+			*(*(mult+i)+j) = (*(*(a+i)+j))*(*(*(b+i)+j));
+		}
+	}
+	return mult;
 
 
 }
 
 void printArray(int **arr, int n){
 	// (2) Implement your printArray function here
+	int i = 0, j = 0;
+	for(i; i<n; i++){
+		for(j; j<n; j++){
+			printf("%d\t",*(*(arr+i)+j));
+		}
+		printf("\n");
+	}
+	printf("\n");
 
 }
 
 int main() {
-	int n = 0;
+	int n = 5;
 	int **matA, **matB, **matC;
 	// (1) Define 2 n x n arrays (matrices). 
-
+	matA = (int**)malloc(n * sizeof(int*));
+	matB = (int**)malloc(n * sizeof(int*));
 
 	// (3) Call printArray to print out the 2 arrays here.
-	
+	printArray(matA, n);
+	printArray(matB, n);
 	
 	//(5) Call matMult to multiply the 2 arrays here.
-	
-	
-	//(6) Call printArray to print out resulting array here.
+	matC = matMult(matA, matB, n);
 
+	//(6) Call printArray to print out resulting array here.
+	printArray(matC, n);
 
 
     return 0;
